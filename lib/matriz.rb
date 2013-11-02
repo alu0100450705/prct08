c9dd4d6c142cd1adce1f03da7e6e49e70a3c310a
Marcos Diaz <alu0100450705@ull.edu.es>
2013-11-02 16:14:56 +0000

    Creando inizialite de la clase Matriz

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,3 +1,10 @@
 class Matriz
 
+
+	def initialize(matriz)
+		@matrix = matriz
+		@filas = matriz.size
+		@columnas = matriz[0].size
+	end
+	
 end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
 require "./lib/matriz.rb"
+
+describe Matriz do
+  before :each do
+    @MatrizA = Matriz.new([[1,1],[2,2]])
+	@MatrizB = Matriz.new([[1,1],[2,2]])
+  end
+  end
