b6a07a0ca30aa929b78d3b87cbc7bf0a54dba76f
Marcos Diaz <alu0100450705@ull.edu.es>
2013-11-02 16:23:39 +0000

    Creando expetativa debe existir filas y columnas

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -3,8 +3,20 @@
 require "./lib/matriz.rb"
 
 describe Matriz do
+
   before :each do
     @MatrizA = Matriz.new([[1,1],[2,2]])
 	@MatrizB = Matriz.new([[1,1],[2,2]])
   end
+  
+  describe "Debe existir " do 
+	it "filas " do
+		(defined?(@MatrizA.filas)).should be_true
+	end
+	
+	it "columnas " do
+		(defined?(@MatrizA.columnas)).should be_true
+	end
+  end
+  
   end
